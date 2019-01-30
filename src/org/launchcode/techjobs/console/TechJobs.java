dd035c0a978e3066e57595a531fb67884e7c99f2
Allen Britt <allen_c_britt@yahoo.com>
2019-01-30 08:42:21 -0500

    Final working to the needs of the application...will revisit over the weekend to add functionality like inputting characters not 0 or 1 errors

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -1,8 +1,6 @@
 package org.launchcode.techjobs.console;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -56,19 +54,21 @@ public class TechJobs {
                 // How does the user want to search (e.g. by skill or employer)
                 String searchField = getUserSelection("Search by:", columnChoices);
 
+
                 // What is their search term?
                 System.out.println("\nSearch term: ");
-                String searchTerm = in.nextLine();
+                String searchTerm = in.nextLine().toUpperCase();
+                //searchTerm.toUpperCase(); // Setting all Search Strings to uppercase
 
                 if (searchField.equals("all")) {
-                    System.out.println("Search all fields not yet implemented.");
+                    //System.out.println("Search all fields not yet implemented.");
+                    printJobs(findByValue(searchTerm));
                 } else {
                     printJobs(JobData.findByColumnAndValue(searchField, searchTerm));
                 }
             }
         }
     }
-
     // ﻿Returns the key of the selected item from the choices Dictionary
     private static String getUserSelection(String menuHeader, HashMap<String, String> choices) {
 
@@ -95,7 +95,6 @@ public class TechJobs {
 
             choiceIdx = in.nextInt();
             in.nextLine();
-
             // Validate user's input
             if (choiceIdx < 0 || choiceIdx >= choiceKeys.length) {
                 System.out.println("Invalid choice. Try again.");
@@ -110,7 +109,52 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
+        /* How TF do I loop through an Arraylist<HashMap> */
+        if (someJobs.size() > 0 ) {
+            // Loop over all of the keys in the hashmap
+            // Try somJobs.keySet()
 
-        System.out.println("printJobs is not implemented yet");
+            for (HashMap<String, String> field : someJobs) {
+
+                for(Map.Entry<String, String> data: field.entrySet()){
+                    System.out.println(data.getKey() + ": "+data.getValue() );
+
+                }
+                System.out.println("*********************");
+            }
+
+        } else System.out.println("Sorry there is nothing that matches your search");
+    }
+
+    //This method will pull in string choice.
+    public static ArrayList<HashMap<String, String>> findByValue(String searchTerm){
+        //  This method will allow the user
+        //  to search across all columns in order to find job input
+        // I need to grab the seach term and loop through each row in the exe file
+        // If a row contains the search term word then I need to print that word.
+        // I could pull every row and put it into an arraylist of hash maps.
+        ArrayList<HashMap<String, String>> allJobs = JobData.findAll();
+        ArrayList<HashMap<String, String>> matchingItems = new ArrayList<>();
+
+        for (HashMap<String,String> row: allJobs){
+
+            for(Map.Entry<String, String> column: row.entrySet()){
+                if (column.getValue().toUpperCase().contains(searchTerm)){
+                    //This is making sure that the hashmap is only being added to
+                    // tmatchingItems onces
+                    //THis will check to see if matchingItems contains row
+                    // if so continue if not add row to matchingItems
+                    if(Arrays.asList(matchingItems).contains(row)){
+                        continue;
+                    }
+                    matchingItems.add(row);
+                }
+            }
+        }
+
+        return matchingItems;
+
+
     }
 }
+
